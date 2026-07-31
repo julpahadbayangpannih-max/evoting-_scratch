41c84d333f24b129fc73d1f1818f2dd8c0bfe5d8
Julfahad Pannih <jordanpannih@gmail.com>
2026-07-31 19:10:59 +0800

    add voters table migration and model

---
+++ database/migrations/2026_07_31_103246_create_voters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  public function up(): void
+{
+    Schema::create('voters', function (Blueprint $table) {
+        $table->id();
+        $table->string('student_id')->unique();
+        $table->string('name');
+        $table->string('course');
+        $table->string('password')->nullable();
+        $table->boolean('is_approved')->default(false);
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voters');
+    }
+};
